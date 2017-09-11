db202d6fc45d13939f0dac38c5e2a4e072759f23
sebi <alariq@gmail.com>
2017-09-11 22:27:27 +0300

    initial commit

---
+++ art/src_names_tolower.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+
+
+opendir(DIR,".") or die "Cannot open . \n";
+
+my @files = readdir(DIR);
+closedir(DIR);
+foreach(@files) {
+
+    if(/\.([a-zA-Z]{3})$/) {
+
+        my $o = $_;
+
+        $o = lc($o);
+
+        system("mv", $_, $o);
+        #print("mv ", $_, " ", $o, "\n");
+    }
+}
+
+#find( \&wanted, [ "./*.mp3" ] );
